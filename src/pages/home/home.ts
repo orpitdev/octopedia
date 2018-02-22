55391889ddabb9bd4a8b697657fc44491d328618
Marco <desenvolvimento@orpit.com.br>
2018-02-22 14:22:40 -0300

    home.ts - audio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,12 +21,12 @@ export class HomePage {
 
   ionViewDidLoad() { 
     window.localStorage.setItem('load', 'true');
-    this.nativeAudio.preloadComplex('botao', 'assets/audio/botao.mp3', 1, 1, 0)
+    this.nativeAudio.preloadComplex('botao_play', 'assets/audio/botao.mp3', 1, 1, 0)
   }
 
   goToRandom(){
-    this.nativeAudio.stop('botao')
-    this.nativeAudio.play('botao')
+    this.nativeAudio.stop('botao_play')
+    this.nativeAudio.play('botao_play')
     this.navCtrl.push('RandomPage');
   }
 }
